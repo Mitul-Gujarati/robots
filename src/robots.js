9b943cc1f7600e752c8933bab3d63208bfe31263
Mitul0908 <85814760+Mitul0908@users.noreply.github.com>
2021-06-22 11:42:15 +0530

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,48 @@
+export const robots = [
+    {
+    id:0,
+    Name: 'Mitul Gujarati',
+    email: 'mitul@gmail.com'
+    },
+
+   {
+    id:1,
+    Name: 'Akash Gupta',
+    email: 'akash@gmail.com'
+   },
+
+   {
+    id:2,
+    Name: 'Ankush Lakhani',
+    email: 'ankush@gmail.com'
+   },
+
+   {
+    id:3,
+    Name: 'Jaimish Golakiya',
+    email: 'jaimish@gmail.com'
+   },
+
+   {
+    id:4,
+    Name: 'Dhruv Gopani',
+    email: 'dhruv@gmail.com'
+   },
+
+   {
+    id:5,
+    Name: 'Jenil Savani',
+    email: 'jenil@gmail.com'
+   },
+   
+   {
+    id:6,
+    Name: 'Darshan Chauhan',
+    email: 'darshan@gmail.com'
+   },
+   {
+    id:7,
+    Name: 'Alish Chabhadiya',
+    email: 'alish@gmail.com'
+   }
+]
